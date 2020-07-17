27b683d437d63cf77347a3cd60f2d43cd28e78e8
Kévin <swaggdepoulefr@gmail.com>
2020-07-18 01:29:52 +0200

    Ajout d'une fonction pour récupérer un mot aléatoire dans le dictionnaire

--- src/core/grammar/Dictionnary.java
+++ src/core/grammar/Dictionnary.java
@@ -3,6 +3,7 @@ package core.grammar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * Classe abstraite dont le but est d'être dérivée par de nouvelles classes
@@ -15,13 +16,22 @@ import java.util.Map;
 public abstract class Dictionnary {
 
 	private Map<Integer, List<Word>> dico;
+	private Random r;
 
 	public Dictionnary() {
 		dico = new HashMap<>();
+		r = new Random();
 		fillDictionnary();
 	}
 
 	public abstract void fillDictionnary();
+	
+	public Word getRandomWord() {
+		Object[] values = dico.values().toArray();
+		@SuppressWarnings("unchecked")
+		List<Word> listWords = (List<Word>) values[r.nextInt(values.length)];
+		return listWords.get(r.nextInt(listWords.size()));
+	}
 
 	public Map<Integer, List<Word>> getDico() {
 		return dico;
