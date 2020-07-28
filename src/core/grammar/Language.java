9f658420cc02dc4cc97414151636b815081279cc
Kévin <swaggdepoulefr@gmail.com>
2020-07-28 03:35:36 +0200

    Ajout d'une mémoire sur les corrections pour optimiser le temps de correction

--- src/core/grammar/Language.java
+++ src/core/grammar/Language.java
@@ -1,6 +1,8 @@
 package core.grammar;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Classe abstraite dont le seul but est d'être dérivée par des classes
@@ -13,16 +15,26 @@ import java.util.List;
 public abstract class Language {
 
 	private Dictionnary dictionnary;
+	private Map<Word, Word> savedCorrections;
 
 	public Language(Dictionnary d) {
 		dictionnary = d;
+		savedCorrections = new HashMap<>();
 	}
 
 	public abstract List<Word> correctSentence(Sentence toCorrect);
 
 	public abstract Word matchWordWithDictionnary(Word w);
+	
+	public Word checkIfWordAlreadyCorrected(Word w) {
+		return savedCorrections.containsKey(w) ? savedCorrections.get(w) : w;
+	}
 
 	public Dictionnary getDictionnary() {
 		return dictionnary;
+	}
+	
+	public Map<Word, Word> getSavedCorrections() {
+		return savedCorrections;
 	}
 }
